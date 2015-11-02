45d43cc6af5d56a8b56e6616802c37ecf3ea912d
amf272 <amf272@cornell.edu>
2015-11-02 17:59:05 -0500

    interpolate polar plot

--- visualization/interpolatePolarPlot.m
+++ visualization/interpolatePolarPlot.m
@@ -2,8 +2,12 @@ function polarPlot = interpolatePolarPlot(velocityMagnitude,polarData)
 
 
 if nargin < 2
-    data = load('../dataCollection/allPolarDiagrams.mat');
+    polarData = load('../dataCollection/allPolarDiagrams.mat');
 end
 
 velInd = velocityMagnitude*2+1;
 
+[x,y,z] = size(polarData.allDiagrams);
+F=griddedInterpolant({1:x,1:y,1:z},polarData.allDiagrams);
+polarPlot= F({velInd,1:y,1:z});
+polarPlot = squeeze(polarPlot);
