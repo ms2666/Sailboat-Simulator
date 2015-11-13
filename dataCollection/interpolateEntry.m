ce644d24b934c8a2aa2bb56ea730001ac48a7b08
amf272 <amf272@cornell.edu>
2015-11-13 16:51:32 -0500

    deleted mat

--- dataCollection/interpolateEntry.m
+++ dataCollection/interpolateEntry.m
@@ -11,8 +11,14 @@ end
 lats = data.g;
 lons = data.f;
 times = data.t(:,1);
-lon = mod(lon,360)+lons(1);
-
+lon = mod(lon,360);
+if lon < windData.f(1)
+    lon = 360 + lon;
+end
+% lat needs to be in the range -59.5 to 59.5
+if abs(lat) > 59.5
+     throw(MException('VerifyOutput:OutOfBounds',sprintf('latitude %d is outside bounds -59.5 to 59.5 (no data outside this)',lat)));
+end
 latind = double((lat-lats(1))/(lats(2)-lats(1)));
 lonind = double((lon-lons(1))/(lons(2)-lons(1)));
 tind = double((time-times(1))/(times(2)-times(1)));
